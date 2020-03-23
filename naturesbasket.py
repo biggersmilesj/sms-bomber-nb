58ddcc0d91c65edc8bb79cdc8fe483430b2add8e
biggersmilesj <blog.biggersmile@gmail.com>
2020-03-24 00:56:15 +0530

    Update naturesbasket.py

--- naturesbasket.py
+++ naturesbasket.py
@@ -2,6 +2,7 @@ import requests
 import sys
 import os
 
-for i in range(int(sys.argv[2])):
-    print(i+1)
-    response = requests.get('https://www.naturesbasket.co.in/Handlers/LoginRegistrationMaster.ashx?sendotp='+sys.argv[1])
+if(int(sys.argv[2])<150):
+    for i in range(int(sys.argv[2])):
+        print(i+1)
+        response = requests.get('https://www.naturesbasket.co.in/Handlers/LoginRegistrationMaster.ashx?sendotp='+sys.argv[1])
